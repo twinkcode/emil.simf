a324c05fa41fb8fac92e9cce4c5ff7d6a2917880
twinkcode <twinkcode@gmail.com>
2023-02-06 17:26:42 +0300

    del comments

--- src/DTO/CalculatePriceDTO.php
+++ src/DTO/CalculatePriceDTO.php
@@ -13,6 +13,7 @@ class CalculatePriceDTO
      * @Assert\NotBlank()
      */
     public Product $product;
+
     /**
      * @return Product
      */
@@ -20,99 +21,10 @@ class CalculatePriceDTO
     {
         return $this->product;
     }
+
     /**
      * @Assert\NotBlank()
      */
     public string $taxNumber;
 
-
-
-//
-//    /**
-//     * @param Product $product
-//     * @return CalculatePriceDTO
-//     */
-//    public function setProduct(Product $product): self
-//    {
-//        $this->product = $product;
-//
-//        return $this;
-//    }
-
-
 }
-//class CalculatePriceDTO
-//{
-//    /**
-//     * @var Product
-//     */
-//    private Product $product;
-//
-//    /**
-//     * @var Country
-//     */
-//    private Country $country;
-//
-//    /**
-//     * @var $taxNumber
-//     */
-//    private string $taxNumber;
-//
-//    /**
-//     * @return string $taxNumber
-//     */
-//    public function getTaxNumber(): string
-//    {
-//        return $this->taxNumber;
-//    }
-//
-//    /**
-//     * @param $taxNumber
-//     * @return CalculatePriceDTO
-//     */
-//    public function setTaxNumber($taxNumber): self
-//    {
-//        $this->taxNumber = $taxNumber;
-//        return $this;
-//    }
-//
-//    /**
-//     * @return Product
-//     */
-//    public function getProduct(): Product
-//    {
-//        return $this->product;
-//    }
-//
-//    /**
-//     * @param Product $product
-//     * @return CalculatePriceDTO
-//     */
-//    public function setProduct(Product $product): self
-//    {
-//        $this->product = $product;
-//
-//        return $this;
-//    }
-//
-//    /**
-//     * @return Country
-//     */
-//    public function getCountry(): Country
-//    {
-//        return $this->country;
-//    }
-//
-//    /**
-//     * @param Country $country
-//     * @return CalculatePriceDTO
-//     */
-//    public function setCountry(Country $country): self
-//    {
-//        $this->country = $country;
-//
-//        return $this;
-//    }
-//
-//
-//}
